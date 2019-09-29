46827fd17bd67283920f70f73d9a4782fd68ca51
Smith-Jessica <jpeters2153@gmail.com>
2019-09-28 23:55:13 -0600

    cleaning up

--- web/assignments.php
+++ web/assignments.php
@@ -23,7 +23,8 @@
 <div class="row p-5 mb-5">
   <a class="btn btn-primary col-8 col-sm-offset-3" href='/team02/teamAct.php'>Week02 Team Activity</a>
 </div>
-<div class="row p-5 mb-5">
+
+<!--<div class="row p-5 mb-5">
   <a class="btn btn-primary col-8 col-sm-offset-3" href=''>Week03 Team Activity</a>
 </div>
 <div class="row p-5 mb-5">
@@ -32,6 +33,6 @@
 <div class="row p-5 mb-5">
   <a class="btn btn-primary col-8 col-sm-offset-3" href=''>Week04 Team Activity</a>
 </div>
-
+-->
 </div>
 </body>
