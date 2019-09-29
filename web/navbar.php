71c5b27313cc96bda8c04195774a0a07c2741205
Smith-Jessica <jpeters2153@gmail.com>
2019-09-28 21:23:05 -0600

    made the navbar appear but the home link isn't working. Gonna try the same fix. ../file_name

--- web/navbar.php
+++ web/navbar.php
@@ -16,7 +16,7 @@
 <nav class="navbar navbar-default">
   <div class="container-fluid">
     <div class="navbar-header">
-      <a class="navbar-brand" href="index.php">Jessica's CS313 Homepage</a>
+      <a class="navbar-brand" href="../index.php">Jessica's CS313 Homepage</a>
     </div>
     <ul class="nav navbar-nav">
       <li class="active"><a href="#">Home</a></li>
