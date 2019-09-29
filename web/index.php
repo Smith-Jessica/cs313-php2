b7020d27bd7c06493a2d29e3f67df92d5091ab4b
Smith-Jessica <jpeters2153@gmail.com>
2019-09-28 20:03:40 -0600

    added code from VS Code that I can't push to remote from there for some reason

--- web/index.php
+++ web/index.php
@@ -1,5 +1,61 @@
 <?php
 
   phpinfo();
+    echo '<a href="/index.html">Home</a>';
+    
+// Add path to countlog.txt file.
+$path = '/Week02/countlog.txt';
+
+// Opens countlog.txt to read the number of hits.
+$file  = fopen( $path, 'r' );
+$count = fgets( $file, 1000 );
+fclose( $file );
+
+// Update the count.
+$count = abs( intval( $count ) ) + 1;
+
+// Output the updated count.
+echo "{$count} hits\n";
+
+// Opens countlog.txt to change new hit number.
+$file = fopen( $path, 'w' );
+fwrite( $file, $count );
+fclose( $file );
 
 ?>
+
+<!DOCTYPE html>
+ <html lang="en">
+ <head>
+   <meta charset="UTF-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+   
+   <script src="homepage.js"></script>
+   <script src="homepage.php"></script>
+   <link rel="stylesheet" type="text/css" href="homepage.css">
+
+   <title>CS313 -- Jessica's Assignments</title>
+</head>
+<body>
+    
+<div class="container-fluid">
+
+    <div class="d-flex justify-content-center align-items-center jumbotron">
+
+        <div id="title">Jessica's Homepage</div>
+
+    </div>
+
+</div>
+
+<div class="container">
+    <div>You are visitor number <?echo $count;?>!</div>
+
+</div>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+
+</body>
+</html>
+
