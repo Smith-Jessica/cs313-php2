00a84b9f5810ab6f50b21124903fe9c3474c6aef
Smith-Jessica <jpeters2153@gmail.com>
2019-10-04 20:10:51 -0600

    changed the class structure to use a constructor. This reduced lines of code for instantiation of the class object as well as allowed me to be able to count the number of instantiated objects

--- web/week03/products.php
+++ web/week03/products.php
@@ -4,24 +4,24 @@
         public $imagelink;
         public $desc;
         public $title;
+        public static $count=0;
+
+        public function __construct($price, $imagelink, $desc, $title)
+        {
+            $this->price= $price;
+            $this->imagelink= $imagelink;
+            $this->desc= $desc;
+            $this->title= $title;
+            cars::$count++;
+            return true;
+        }
     }
 
-    $light = new Product();
-    $light->price = 20;
-    $light->imagelink = 'light.jpg';
-    $light->desc = "The best light for your new Smart Home!";
-    $light->title = "Smart Home Light";
+    $light = new Product(20, 'light.jpg', "The best light for your new Smart Home!", "Smart Home Light");
 
-    $hub = new Product();
-    $hub->price = 30;
-    $hub->imagelink = 'hub.jpg';
-    $hub->desc = "Google's Hub with Google Assistant will give you the control you want for your Smart Home";
-    $hub->title = "Google Hub";
+    $hub = new Product(30, 'hub.jpg', "Google's Hub with Google Assistant will give you the control you want for your Smart Home", "Google Hub");
 
-    $alexa = new Product();
-    $alexa->price = 30;
-    $alexa->imagelink = 'alexa.jpg';
-    $alexa->desc = "Amazon Alexa gives you complete control. Better than our competitors, who will remain nameless *cough*Google*cough*";
-    $alexa->title = "Amazon Alexa";
+    $alexa = new Product(30, 'alexa.jpg', "Amazon Alexa gives you complete control. Better than our competitors, who will remain nameless *cough*Google*cough*", "Amazon Alexa");
+ 
 
 ?>
