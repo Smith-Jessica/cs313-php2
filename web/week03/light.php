f127a846d5e3e1f7698dd80e3b29d48c104873ac
Smith-Jessica <jpeters2153@gmail.com>
2019-10-05 17:08:27 -0600

    Trying to fix an issue on browse with the detail link not working. And with light.php testing the echo

--- web/week03/light.php
+++ web/week03/light.php
@@ -23,9 +23,9 @@ include 'products.php';
         <div class="row">
         <div class="col-sm-6 d-flex justify-content-center">
         <div class="card" style="width:70rem">
-        <img class="card-img-top" src= <?php echo $allProducts[0]->imagelink ?> alt="Card image" style="width:100%">
+        <img class="card-img-top" src= <?php echo $allProducts[0]->imagelink; ?> alt="Card image" style="width:100%">
         <div class="card-body">
-        <h4 class="card-title"> . $y->title </h4>
+        <h4 class="card-title"> <?php echo $allProducts[0]->title; ?> </h4>
         <p class="card-text"> . $y->desc . </p>
         <a href="" class="btn btn-primary">See Product</a>
         </div> </div> </div>
