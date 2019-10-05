139d73bd8d90d1a6057e53688279fe401d863267
Smith-Jessica <jpeters2153@gmail.com>
2019-10-04 21:12:52 -0600

    echoing out the var_dump to see what's going on here

--- web/week03/browse.php
+++ web/week03/browse.php
@@ -20,10 +20,10 @@
     include 'products.php';
 
 for($x = 0; $x < Products::$count; $x++){
-  //$y = new Product(); -- This is my backup plan if the below does not work. 
+  //$y = new Product(); will need a for loop to instantiate $y and then probably need to delete it at the end of each iteration -- This is my backup plan if the below does not work. 
 //gonna try the below method first. See if it transfers over all of the Object info to $y
   $y = $allProducts[$x];
-  var_dump($y);
+  echo var_dump($y);
 echo "<div class=\"card\" style=\"width:400px\">" .
       "<img class=\"card-img-top\" src= alt=\"Card image\" style=\"width:100%\">" .
       "<div class=\"card-body\">" .
@@ -31,7 +31,8 @@ echo "<div class=\"card\" style=\"width:400px\">" .
         "<p class=\"card-text\">" . $y->desc . "</p>" .
         "<a href=\"" . $y->detailLink . "\" class=\"btn btn-primary\">See Profile</a>" .
       "</div> </div>";
-  }
+ // unset($y);
+    }
   ?>
   </div>
 </body>
