d189ff1f9936660e7cde1966750d210c87e9d852
Smith-Jessica <jpeters2153@gmail.com>
2019-10-05 12:47:42 -0600

    found an error in some borrowed code. Fixed the issue. Now maybe it will work?

--- web/week03/products.php
+++ web/week03/products.php
@@ -1,6 +1,49 @@
 <?php
-include "collections.php";
+//collection class
+class Collection 
+{
+    private $items = array();
 
+    public function addItem($obj, $key = null) {
+            if ($key == null) {
+                $this->items[] = $obj;
+            }
+            else {
+                if (isset($this->items[$key])) {
+                    return; //throw new KeyHasUseException("Key $key already in use."); -- can add this file later
+                }
+                else {
+                    $this->items[$key] = $obj;
+                }
+            }
+    }
+
+  /*  public function deleteItem($key) {
+        if (isset($this->items[$key])) {
+            unset($this->items[$key]);
+        }
+        else {
+            throw new KeyInvalidException("Invalid key $key.");
+        }
+    } --can add this functionality later*/
+
+    public function getItem($key) {
+        if (isset($this->items[$key])) {
+            return $this->items[$key];
+        }
+        else {
+            return; //throw new KeyInvalidException("Invalid key $key.");
+        }
+    }
+    public function keys() {
+        return array_keys($this->items);
+    }
+    public function length() {
+        return count($this->items);
+    }
+    
+}
+//product class
     class Product {
         public $price;
         public $imagelink;
@@ -16,7 +59,7 @@ include "collections.php";
             $this->desc= $desc;
             $this->title= $title;
             $this->$detailLink = $detailLink;
-            cars::$count++;
+            Product::$count++;
             return true;
         }
         public function _construct()
@@ -30,15 +73,6 @@ include "collections.php";
     $allProducts->addItem(new Product(20, 'light.jpg', "The best light for your new Smart Home!", "Smart Home Light", 'light.php'), 0);
     $allProducts->addItem(new Product(30, 'hub.jpg', "Google's Hub with Google Assistant will give you the control you want for your Smart Home", "Google Hub", 'hub.php'), 1);
     $allProducts->addItem(new Product(30, 'alexa.jpg', "Amazon Alexa gives you complete control. Better than our competitors, who will remain nameless *cough*Google*cough*", "Amazon Alexa", 'alexa.php'), 2);
-
-   // $light = new Product(20, 'light.jpg', "The best light for your new Smart Home!", "Smart Home Light", 'light.php');
-
-//    $hub = new Product(30, 'hub.jpg', "Google's Hub with Google Assistant will give you the control you want for your Smart Home", "Google Hub", 'hub.php');
-
-   // $alexa = new Product(30, 'alexa.jpg', "Amazon Alexa gives you complete control. Better than our competitors, who will remain nameless *cough*Google*cough*", "Amazon Alexa", 'alexa.php');
- 
-    //$allProducts = array($light, $hub, $alexa); --replacing array with collections class
-
 
 
 ?>
