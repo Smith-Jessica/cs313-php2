f1747d232bc019083ee3e8e2a09a5be8f9d70f8c
Smith-Jessica <jpeters2153@gmail.com>
2019-10-05 12:27:51 -0600

    Trying to comment everything out but the simplest code to find the 500 error

--- web/week03/browse.php
+++ web/week03/browse.php
@@ -15,28 +15,29 @@
   <h1>Shop Here</h1>
   <div class="container">
     <?php
-    include 'products.php';
-    include 'collections.php';
-    $y = new Product();
-    $y = Collection::$allProducts.getItem($x);
+   // include 'products.php';
+  //  include 'collections.php';
+  //  $y = new Product();
+  //  $y = Collection::$allProducts.getItem($x);
+  $y = 2;
     echo $y;
-    echo var_dump($y);
+  //  echo var_dump($y);
 
-      for($x = 0; $x < Collection::$allProducts.length(); $x++){
+    /*  for($x = 0; $x < Collection::$allProducts.length(); $x++){
         // will need a for loop to instantiate $y and then probably need to delete it at the end of each iteration -- This is my backup plan if the below does not work. 
         //$y = $allProducts[$x];
         //echo var_dump($y);
         $y = new Product();
         $y = Collection::$allProducts.getItem($x);
 
-      /*  echo "<div class=\"card\" style=\"width:400px\">" .
+        echo "<div class=\"card\" style=\"width:400px\">" .
             "<img class=\"card-img-top\" src= alt=\"Card image\" style=\"width:100%\">" .
             "<div class=\"card-body\">" .
               "<h4 class=\"card-title\">" . $y->title . "</h4>" .
               "<p class=\"card-text\">" . $y->desc . "</p>" .
               "<a href=\"" . $y->detailLink . "\" class=\"btn btn-primary\">See Profile</a>" .
             "</div> </div>";
-*/
+
     echo <<<EOT 
         <div class="card" style="width:400px">
         <img class="card-img-top" src= alt="Card image" style="width:100%">
@@ -49,6 +50,7 @@
     EOT;          
           unset($y);
       }
+      */
       ini_set('display_errors', 1);
   ?>
   </div>
