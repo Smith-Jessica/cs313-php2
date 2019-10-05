fa6b30ed065003680d70494561e51c6215eea223
Smith-Jessica <jpeters2153@gmail.com>
2019-10-04 20:01:51 -0600

    Added some products to the products.php file

--- web/week03/browse.php
+++ web/week03/browse.php
@@ -6,13 +6,30 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" type="text/css" href="homepage.css">
+  <link rel="stylesheet" type="text/css" href="">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
   <script src="week3.js"></script>
-  <title>Week 03 -- Shopping Cart</title>
+  <title>Browse Items</title>
 </head>
 
 <body>
+<div class="container">
+
+LOOP HERE
+<?php
+foreach($key as $value){
+
+  <div class="card" style="width:400px">
+      <img class="card-img-top" src= alt="Card image" style="width:100%">
+      <div class="card-body">
+        <h4 class="card-title">John Doe</h4>
+        <p class="card-text">Some example text some example text. John Doe is an architect and engineer</p>
+        <a href="#" class="btn btn-primary">See Profile</a>
+      </div>
+    </div>
+  }
+  ?>
+  </div>
 </body>
--- web/week03/products.php
+++ web/week03/products.php
@@ -4,8 +4,24 @@
         public $imagelink;
         public $desc;
         public $title;
-
     }
 
+    $light = new Product();
+    $light->price = 20;
+    $light->imagelink = 'light.jpg';
+    $light->desc = "The best light for your new Smart Home!";
+    $light->title = "Smart Home Light";
+
+    $hub = new Product();
+    $hub->price = 30;
+    $hub->imagelink = 'hub.jpg';
+    $hub->desc = "Google's Hub with Google Assistant will give you the control you want for your Smart Home";
+    $hub->title = "Google Hub";
+
+    $alexa = new Product();
+    $alexa->price = 30;
+    $alexa->imagelink = 'alexa.jpg';
+    $alexa->desc = "Amazon Alexa gives you complete control. Better than our competitors, who will remain nameless *cough*Google*cough*";
+    $alexa->title = "Amazon Alexa";
 
 ?>
