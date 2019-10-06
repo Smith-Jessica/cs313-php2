ecf095edc9a1f6645aed871d9f4abc2b6130cc62
Smith-Jessica <jpeters2153@gmail.com>
2019-10-05 22:08:53 -0600

    fixing an issue in addedtocart.php

--- web/week03/addedtocart.php
+++ web/week03/addedtocart.php
@@ -5,10 +5,14 @@ session_start();
 include 'navbar.php';
 include 'products.php';
 
-$allProducts = unserialize($_SESSION["allProducts"]);
-
+        $allProducts = new Collection();
+    
+        $allProducts->addItem(new Product(20, 'light.jpg', "The best light for your new Smart Home!", "Smart Home Light", 'light.php'), 0);
+        $allProducts->addItem(new Product(30, 'hub.jpg', "Google's Hub with Google Assistant will give you the control you want for your Smart Home", "Google Hub", 'hub.php'), 1);
+        $allProducts->addItem(new Product(30, 'alexa.jpg', "Amazon Alexa gives you complete control. Better than our competitors, who will remain nameless *cough*Google*cough*", "Amazon Alexa", 'alexa.php'), 2);
+   
 $title = $_GET["title"];
-for($x = 0; $x < $allProducts->count(); $x++){
+for($x = 0; $x < $allProducts->length(); $x++){
     $y = $allProducts[$x];
     if($y->$title == $title) {
         if(isset($_SESSION["cart"])) {
