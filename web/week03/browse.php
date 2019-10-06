c1b927fb2057f820de7efd46fea52f94814bcb3f
Smith-Jessica <jpeters2153@gmail.com>
2019-10-05 18:31:28 -0600

    still not working. 500 error

--- web/week03/browse.php
+++ web/week03/browse.php
@@ -27,8 +27,8 @@ include 'products.php';
         $allProducts->addItem(new Product(30, 'hub.jpg', "Google's Hub with Google Assistant will give you the control you want for your Smart Home", "Google Hub", 'hub.php'), 1);
         $allProducts->addItem(new Product(30, 'alexa.jpg', "Amazon Alexa gives you complete control. Better than our competitors, who will remain nameless *cough*Google*cough*", "Amazon Alexa", 'alexa.php'), 2);
 
-      echo $allProducts[0]->detailLink;
-      
+      echo "$allProducts[0]->detailLink";
+
       for($x = 0; $x < $allProducts->length(); $x++){
         $y = $allProducts->getItem($x);
 
