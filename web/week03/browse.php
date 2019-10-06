508e64137192dfa66703ca5d5f16894aca63c5ad
Smith-Jessica <jpeters2153@gmail.com>
2019-10-05 18:56:02 -0600

    still trying to fix the browse issue

--- web/week03/browse.php
+++ web/week03/browse.php
@@ -21,7 +21,7 @@ session_start();
 include 'navbar.php';
 include 'products.php';
 
-        $light = new Product(20, 'light.jpg', "The best light for your new Smart Home!", "Smart Home Light", 'light.php');
+        $light = new Product(20, 'light.jpg', "The best light for your new Smart Home!", "Smart Home Light", "light.php");
         $hub = new Product(30, 'hub.jpg', "Google's Hub with Google Assistant will give you the control you want for your Smart Home", "Google Hub", 'hub.php');
         $alexa = new Product(30, 'alexa.jpg', "Amazon Alexa gives you complete control. Better than our competitors, who will remain nameless *cough*Google*cough*", "Amazon Alexa", 'alexa.php');
         $allProducts = array($light, $hub, $alexa);
@@ -30,7 +30,7 @@ include 'products.php';
         $allProducts->addItem(new Product(30, 'hub.jpg', "Google's Hub with Google Assistant will give you the control you want for your Smart Home", "Google Hub", 'hub.php'), 1);
         $allProducts->addItem(new Product(30, 'alexa.jpg', "Amazon Alexa gives you complete control. Better than our competitors, who will remain nameless *cough*Google*cough*", "Amazon Alexa", 'alexa.php'), 2);
 */
-      echo $allProducts[0]->title;
+      echo $allProducts[0]->detailLink;
 
       for($x = 0; $x < count($allProducts); $x++){
         $y = $allProducts[$x];
