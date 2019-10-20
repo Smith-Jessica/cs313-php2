796a7890438a93c7b3761a852f023221c539c8ab
Smith-Jessica <jpeters2153@gmail.com>
2019-10-19 18:32:04 -0600

    stupid

--- db/myDb.sql
+++ db/myDb.sql
@@ -20,7 +20,8 @@ CREATE TABLE products (
     description TEXT,
     image       TEXT,
     price       FLOAT(2),
-    category_id INTEGER REFERENCES categories(id));
+    category_id INTEGER REFERENCES categories(id),
+    detail_pg   TEXT);
     
 CREATE TABLE cart_product (
   cart_id    INTEGER REFERENCES cart(id) ON UPDATE CASCADE ON DELETE CASCADE,
@@ -33,8 +34,9 @@ INSERT INTO cart DEFAULT VALUES;
 INSERT INTO users VALUES(DEFAULT,'test', 'user', 'test', 'test', 1);
 
 INSERT INTO categories VALUES(DEFAULT,'Hub', 'This is a category for all the different command hubs') RETURNING id;
-INSERT INTO products VALUES(DEFAULT,'Google Home', 'This is Googles Command hub', 'hub.jpg', 129.00, 1);
-INSERT INTO products VALUES(DEFAULT,'Amazon Alexa Echo', 'This is Amazon Alexa, the Command hub', 'alexa.jpg', 149.00, 1);
+INSERT INTO products VALUES(DEFAULT,'Google Home', 'This is Googles Command hub', 'hub.jpg', 129.00, 1, 'hub.php');
+INSERT INTO products VALUES(DEFAULT,'Amazon Alexa Echo', 'This is Amazon Alexa, the Command hub', 'alexa.jpg', 149.00, 1, 'alexa.php');
 
 INSERT INTO categories VALUES(DEFAULT,'Light', 'This is a category for everything lights') RETURNING id;
-INSERT INTO products VALUES(DEFAULT,'Light', 'This is a smart light', 'light.jpg', 30.00, 2);
+INSERT INTO products VALUES(DEFAULT,'Light', 'This is a smart light', 'light.jpg', 30.00, 2, 'light.php');
+
--- web/week03/browse.php
+++ web/week03/browse.php
@@ -50,14 +50,16 @@ foreach ($db->query('SELECT * FROM products') as $row)
   $img = $row['image'];
   $price = $row['price'];
   $category = $row['category'];
-  echo '<br/>';
+  $detail_pg = $row['details'];
+
+  $allProducts->addItem(new Product($price, $img, $desc, $title, $detail_pg), $index);
 }
 
 include 'navbar.php';
 include 'products.php';
         $allProducts = new Collection();
     
-        $allProducts->addItem(new Product(20, 'light.jpg', "The best light for your new Smart Home!", "Smart Home Light", 'light.php'), 0);
+        $allProducts->addItem(new Product($price, $img, $desc, $title, 'light.php'), 0);
         $allProducts->addItem(new Product(30, 'hub.jpg', "Google's Hub with Google Assistant will give you the control you want for your Smart Home", "Google Hub", 'hub.php'), 1);
         $allProducts->addItem(new Product(30, 'alexa.jpg', "Amazon Alexa gives you complete control. Better than our competitors, who will remain nameless *cough*Google*cough*", "Amazon Alexa", 'alexa.php'), 2);
         
