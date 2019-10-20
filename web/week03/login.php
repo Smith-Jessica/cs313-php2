cf7bb807bad27208ce216237ac1f2130eb744636
Smith-Jessica <jpeters2153@gmail.com>
2019-10-19 21:27:02 -0600

    debugging time. 500 error when submit login form

--- web/week03/login.php
+++ web/week03/login.php
@@ -24,9 +24,11 @@ try
             die();
           }
 
-        $username = $_POST["username"];
-        $password = $_POST["password"];
+        
   if(isset($_POST["username"]) && isset($_POST["password"])){
+    $username = $_POST["username"];
+    $password = $_POST["password"];
+
         $stmt = $db->prepare("SELECT * FROM user WHERE username=:username");
         $stmt->execute(['username' => $username]);
         $user = $stmt->fetch();
