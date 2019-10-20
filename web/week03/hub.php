3bb84fe3783a0d312080ad8daf230a0833bc9247
Smith-Jessica <jpeters2153@gmail.com>
2019-10-19 18:54:11 -0600

    adding php for the details pages to query from the db

--- web/week03/hub.php
+++ web/week03/hub.php
@@ -22,24 +22,29 @@ session_start();
   <div class="container">
   <?php 
         include 'products.php';
-        $allProducts = new Collection();
-    
-        $allProducts->addItem(new Product(20, 'light.jpg', "The best light for your new Smart Home!", "Smart Home Light", 'light.php'), 0);
-        $allProducts->addItem(new Product(30, 'hub.jpg', "Google's Hub with Google Assistant will give you the control you want for your Smart Home", "Google Hub", 'hub.php'), 1);
-        $allProducts->addItem(new Product(30, 'alexa.jpg', "Amazon Alexa gives you complete control. Better than our competitors, who will remain nameless *cough*Google*cough*", "Amazon Alexa", 'alexa.php'), 2);
-   
-        $y = $allProducts->getItem(1);
+        foreach ($db->query('SELECT * FROM products WHERE detail_pg == hub.php') as $row)
+        {
+          $title =  $row['title'];
+          $desc = $row['description'];
+          $img = $row['image'];
+          $price = $row['price'];
+          $category = $row['category'];
+          $detail_pg = $row['detail_pg'];
+
+         // $allProducts->addItem(new Product($price, $img, $desc, $title, $detail_pg), $index);
+        }
+        
 
         echo "<div class=\"container\">";
         echo "<div class=\"row\">";
         echo "<div class=\"col-sm-6 d-flex justify-content-center\">";
         echo "<div class=\"card\" style=\"width:70rem\">";
-        echo "<img class=\"card-img-top\" src=\"" . $y->imagelink . "\" alt=\"Card image\" style=\"width:100%\">";
+        echo "<img class=\"card-img-top\" src=\"" . $img . "\" alt=\"Card image\" style=\"width:100%\">";
         echo "<div class=\"card-body\">";
-        echo "<h4 class=\"card-title\">$" . $y->price . ".00</h4>";
-        echo "<h4 class=\"card-title\">" . $y->title . "</h4>";
-        echo "<p class=\"card-text\">" . $y->desc . "</p>";
-        echo "<a href=\"addedtocart.php?title=" . $y->title ."\" class=\"btn btn-primary\">Add to Cart</a>";
+        echo "<h4 class=\"card-title\">$" . $price . ".00</h4>";
+        echo "<h4 class=\"card-title\">" . $title . "</h4>";
+        echo "<p class=\"card-text\">" . $desc . "</p>";
+        echo "<a href=\"addedtocart.php?title=" . $title ."\" class=\"btn btn-primary\">Add to Cart</a>";
         echo "</div> </div> </div>";
  ?>
   </div>
