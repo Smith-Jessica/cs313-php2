e1c77e3614e41a974dd1b31bc56eab8a75cbee12
Smith-Jessica <jpeters2153@gmail.com>
2019-10-19 19:23:08 -0600

    I have no idea why this isn't showing up?

--- web/week03/hub.php
+++ web/week03/hub.php
@@ -61,7 +61,7 @@ session_start();
         $stmt->execute(['detail_pg' => $detail_pg]); 
         $product = $stmt->fetch();
 
-        echo $product->title;
+        echo $product;
 
         echo "<div class=\"container\">";
         echo "<div class=\"row\">";
