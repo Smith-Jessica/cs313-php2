0aeb03fd42f3f4701edd4d54b33a12ff2b6ff9d5
Smith-Jessica <jpeters2153@gmail.com>
2019-10-29 21:50:13 -0600

    Not sure what the cart error is. It's saying the session variable cart is not set

--- web/week03/navbar.php
+++ web/week03/navbar.php
@@ -23,6 +23,7 @@
       <li class="active"><a href="cart.php">View Cart</a></li>
       <li class="active pull-right"><a class="btn btn-success" href="login.php">Login</a></li>
       <li class="active pull-right"><a class="btn btn-success" href="signup.php">Sign Up</a></li>
+      <li class="active pull-right"><a class="btn btn-success" href="logout.php">Log Out</a></li>
     </ul>
   </div>
 </nav>
