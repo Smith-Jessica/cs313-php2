e75bd5bee6d70a312ddd184fdcd7b264d6a54c31
Smith-Jessica <jpeters2153@gmail.com>
2019-11-02 16:07:42 -0600

    figuring out some issues with logging out

--- web/week03/logout.php
+++ web/week03/logout.php
@@ -2,13 +2,17 @@
 // Start the session
 session_start();
 
-if (isset( $_SESSION['username']) ) {
-  // Grab user data from the database using the user_id
-  // Let them access the "logged in only" pages
+if (isset($_SESSION['username'])) {
+ 
   unset($_SESSION["username"]);
   unset($_SESSION["cart"]);
 
   // Redirect them to the login page
   header("Location: https://floating-ocean-98131.herokuapp.com/week03/login.php");
+}
+else
+{
+    // Redirect them to the login page
+    header("Location: https://floating-ocean-98131.herokuapp.com/week03/login.php");
 }
 ?>
