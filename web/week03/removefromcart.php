1d48c4fa78fb99a5e9b75022408b4a20969d76c4
Smith-Jessica <jpeters2153@gmail.com>
2019-11-02 15:27:22 -0600

    debugging and fixing a 500 error

--- web/week03/removefromcart.php
+++ web/week03/removefromcart.php
@@ -3,18 +3,22 @@
 session_start();
 
 include 'navbar.php';
-include 'products.php';
+
 
 if(isset($_SESSION["cart"])) { //if the user is logged in
     //get the all product ids where the cart $id == $_SESSION['cart]
-    //echo "the session cart is set, going to get the data from db\n";
-      try {   
-        $result = $db->prepare("DELETE FROM orders WHERE cart_id= :cartid AND product_id = :prodid");
-        $result->bindParam('cartid', $_SESSION['cart']);
-        $result->bindParam('prodid', $_GET["id"]);
-        $result->execute();
-        
+    echo "the session cart is set, going to get the data from db\n";
+      try {
         $product_id = $_GET["id"];
+        $result = $db->prepare("DELETE FROM orders WHERE cart_id= :cartid AND product_id = :prodid");
+        echo "the prepare statement is fine\n";
+        $result->bindParam('cartid', $_SESSION['cart']);
+        echo "bindParam cart_id is fine\n";
+        $result->bindParam('prodid', $product_id);
+        echo "bindParam product_id is fine\n";
+        $result->execute();
+        echo "execution is fine\n";
+        
         echo "<h1>The product has been removed from your cart!</h1>";
       }
     catch (Exception $e) {
