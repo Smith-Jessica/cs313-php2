b83fe7d366a935ef9951b55c4f947e6a5ee5ce0c
Smith-Jessica <jpeters2153@gmail.com>
2019-11-02 15:32:38 -0600

    remove from cart function now works. removed debugging statements

--- web/week03/removefromcart.php
+++ web/week03/removefromcart.php
@@ -26,19 +26,14 @@ catch (PDOException $ex)
 }
 
 if(isset($_SESSION["cart"])) { //if the user is logged in
-    //get the all product ids where the cart $id == $_SESSION['cart]
-    echo "the session cart is set, going to get the data from db\n";
+   
       try {
         $product_id = $_GET["id"];
-        echo "the GET variable is fine\n";
         $result = $db->prepare("DELETE FROM orders WHERE cart_id= :cartid AND product_id = :prodid");
-        echo "the prepare statement is fine\n";
         $result->bindParam('cartid', $_SESSION['cart']);
-        echo "bindParam cart_id is fine\n";
         $result->bindParam('prodid', $product_id);
-        echo "bindParam product_id is fine\n";
         $result->execute();
-        echo "execution is fine\n";
+        
         
         echo "<h1>The product has been removed from your cart!</h1>";
       }
